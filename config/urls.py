a32d0e1d00e4eee75d202cb4c91561c5e12a5b5c
kenta138 <garioji@gmail.com>
2026-07-13 09:15:01 +0900

    Djangoユーザー画面のヘッダーからリダイレクト

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/dashboard/', permanent=False)),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('clients.urls')),
