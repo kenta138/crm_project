87f80eda421a179167ef07a4bb0f1bb640c793cf
kenta138 <garioji@gmail.com>
2026-07-28 19:52:53 +0900

    管理サイトの取引先編集で無効ラベルを選択できないよう修正

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Client, SystemSetting
-
+from labels.models import Label
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'phase', 'assigned_user', 'last_contact_date')
@@ -8,6 +8,10 @@ class ClientAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email')
     filter_horizontal = ('labels',)
 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'labels':
+            kwargs['queryset'] = Label.objects.filter(is_active=True)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
 @admin.register(SystemSetting)
 class SystemSettingAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
